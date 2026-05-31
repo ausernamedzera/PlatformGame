463fd78619c8b282cc9529254105c619cd1d1258
ausernamedzera <hellodarkimthelight@hotmail.com>
2026-05-31 23:50:05 +0300

    set_background

--- objects/game.py
+++ objects/game.py
@@ -4,4 +4,4 @@ from constants import *
 class Game(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-
+        arcade.set_background_color(arcade.color.SKY_BLUE)
