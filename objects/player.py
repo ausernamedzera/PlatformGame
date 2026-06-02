c8be81a803edf923e53db2221804c18bcdeb463f
ausernamedzera <hellodarkimthelight@hotmail.com>
2026-06-02 23:59:34 +0300

    center_x_y

--- objects/player.py
+++ objects/player.py
@@ -5,3 +5,5 @@ class Player(arcade.Sprite):
     def __init__(self):
         super().__init__()
         self.texture = arcade.make_soft_circle_texture(40, arcade.color.BLUE)
+        self.center_x = 100
+        self.center_y = 200
