f3bbfb6cb9ec1ae9be507cee404933afc3c408fc
ausernamedzera <hellodarkimthelight@hotmail.com>
2026-06-02 23:58:58 +0300

    class_player_texture

--- objects/player.py
+++ objects/player.py
@@ -4,3 +4,4 @@ from constants import *
 class Player(arcade.Sprite):
     def __init__(self):
         super().__init__()
+        self.texture = arcade.make_soft_circle_texture(40, arcade.color.BLUE)
