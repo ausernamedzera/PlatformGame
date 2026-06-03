d7c1385bef738cb1910c5ade68be0fb5fd0ce3d9
ausernamedzera <hellodarkimthelight@hotmail.com>
2026-06-04 00:09:43 +0300

    syntax_fixed

--- objects/game.py
+++ objects/game.py
@@ -29,10 +29,9 @@ class Game(arcade.Window):
         )
 
     def on_draw(self):
-        def on_draw(self):
-            self.clear()
-            self.platform_list.draw()
-            arcade.draw_circle_filled(self.player.center_x, self.player.center_y, 20, arcade.color.BLUE)
+        self.clear()
+        self.platform_list.draw()
+        arcade.draw_circle_filled(self.player.center_x, self.player.center_y, 20, arcade.color.BLUE)
 
     def on_update(self, delta_time):
         if self.physics_engine:
