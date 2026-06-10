a2c5668131a7325c143640ab6bc28e569ab46c9b
ausernamedzera <hellodarkimthelight@hotmail.com>
2026-06-10 22:20:09 +0300

    worm_coordinates

--- objects/enemy.py
+++ objects/enemy.py
@@ -4,3 +4,5 @@ import random
 class Enemy:
     def __init__(self, x, y):
         super().__init__(":resources:images/enemies/wormGreen.png", scale = 0.5)
+        self.center_x = x
+        self.center_y = y
