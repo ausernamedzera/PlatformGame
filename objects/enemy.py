fa1e52b8d04155e7aa5743df6ec219ccbface780
ausernamedzera <hellodarkimthelight@hotmail.com>
2026-06-12 23:54:16 +0300

    bug_fix

--- objects/enemy.py
+++ objects/enemy.py
@@ -3,7 +3,9 @@ import random
 
 class Enemy:
     def __init__(self, x, y):
-        super().__init__(":resources:images/enemies/wormGreen.png", scale = 0.5)
+        super().__init__()
+        self.texture = arcade.load_texture(":resources:images/enemies/wormGreen.png")
+        self.scale = 0.5
         self.center_x = x
         self.center_y = y
         self.change_x = random.choice([-2, 2])
