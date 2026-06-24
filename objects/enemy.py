1c6b810c8aa79d19965c80d6bc4ef2563bdf384f
ausernamedzera <hellodarkimthelight@hotmail.com>
2026-06-24 23:29:05 +0300

    enemy_physics_engine

--- objects/enemy.py
+++ objects/enemy.py
@@ -2,13 +2,18 @@ import arcade
 import random
 
 class Enemy(arcade.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, platform_list):
         texture = arcade.load_texture(":resources:images/enemies/wormGreen.png")
         super().__init__(texture)
         self.center_x = x
         self.center_y = y
         self.change_x = random.choice([-2, 2])
         self.start_x = x
+        self.physics_engine = arcade.PhysicsEnginePlatformer(
+            self,
+            gravity_constant = 1,
+            walls = platform_list
+        )
 
     def update(self):
         self.center_x += self.change_x
