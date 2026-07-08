a42cea3d9116ebf6d0eeabaac2f4516c588e119a
ausernamedzera <hellodarkimthelight@hotmail.com>
2026-07-08 23:53:39 +0300

    restrat_the_game_v2

--- objects/game.py
+++ objects/game.py
@@ -57,6 +57,7 @@ class Game(arcade.Window):
             if key == arcade.key.ESCAPE:
                 arcade.exit()
             elif key == arcade.key.R:
+                setup()
 
     def on_key_release(self, key, modifiers):
         if key == arcade.key.LEFT or key == arcade.key.RIGHT:
